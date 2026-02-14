a50b2846cf5c0692967c929b321e2c55552d5874
Fabio Albertin <albertin@gmail.com>
2026-02-14 01:45:03 +0000

    Initial Commit

---
+++ fence-action/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fence-action
+//
+//  Created by Fabio Albertin on 14/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fence-action/fence_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  fence_actionApp.swift
+//  fence-action
+//
+//  Created by Fabio Albertin on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct fence_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
