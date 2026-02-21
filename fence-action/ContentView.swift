d2740d2b2a46828ca4a4c85f0b7af35f4469649c
Fabio Albertin <albertin@gmail.com>
2026-02-21 18:39:13 +0000

    App template plus config

--- fence-action/ContentView.swift
+++ fence-action/ContentView.swift
@@ -6,16 +6,13 @@
 //
 
 import SwiftUI
+import MapboxMaps
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        let center = CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0)
+                Map(initialViewport: .camera(center: center, zoom: 2, bearing: 0, pitch: 0))
+                    .ignoresSafeArea()
     }
 }
 
